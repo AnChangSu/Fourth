1c8a578ab6dbc4d8be71e246e128a13812bb4247
安昌 <ac1986.love@163.com>
2017-08-11 10:51:17 +0800

    提交组件4

---
+++ Fourth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fourth
+//
+//  Created by 安昌 on 2017/8/11.
+//  Copyright © 2017年 安昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fourth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fourth
+//
+//  Created by 安昌 on 2017/8/11.
+//  Copyright © 2017年 安昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
